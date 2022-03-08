56a6afc17a2a574f6c89f97c60ed3f0cbcfebaac
Mamun-swe <mamun.swe.277@gmail.com>
2022-03-08 11:52:15 +0600

    Redis removed

--- backend/api/routes/public.route.js
+++ backend/api/routes/public.route.js
@@ -1,13 +1,11 @@
 
 const publicRouter = require("express").Router()
-const cache = require("../cache")
-
 const category = require("../controllers/public/category.controller")
 const researcher = require("../controllers/public/researcher.controller")
 
 
 /* Public routes */
-publicRouter.get("/category", cache.Category, category.index)
+publicRouter.get("/category", category.index)
 publicRouter.get("/researcher", researcher.index)
 publicRouter.get("/researcher/:username", researcher.show)
 publicRouter.get("/researcher/publications/:username", researcher.publications)
